5b32ccd498d056bdf6c79ce1fe46b900fc54b984
Nomor <thirimyat68@gmail.com>
2023-09-16 23:57:40 +0630

    added search bar

---
+++ src/components/data.js
@@ -0,0 +1,22 @@
+import course1 from '../images/1.png'
+import course2 from '../images/2.png'
+let allCourses = [
+    {
+        name: 'HTML5 ,CSS3 ,Java Script Basic to Advanced',
+        owner: 'Nora',
+        price: 40000,
+        img: course1,
+        catagory: 'website development',
+        link: '/basic'
+    },
+    {
+        name: 'React Js',
+        owner: 'Nora',
+        price: 40000,
+        img: course2,
+        catagory: 'website',
+        link: '/react'
+    }
+]
+
+export default allCourses;
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,17 +1,77 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
 import './navbar.css'
+import noraLogoOne from '../../images/nora-logo-2.png'
+import { BsSearch } from 'react-icons/bs'
+import allCourses from '../data'
+
 
 const NavBar = () => {
+    let [searchValue, setSearchValue] = useState('');
+
+    let handleSearch = (e) => {
+        setSearchValue(e.target.value);
+    }
+
+    useEffect(() => {
+        let searchResult = document.querySelector('.search-result');
+        let resultContainerDiv = document.createElement('div');
+        resultContainerDiv.setAttribute('class', 'result-container-div')
+        while (searchResult.firstChild) {
+            searchResult.removeChild(searchResult.firstChild)
+        }
+        if (searchValue.length > 2) {
+            let filteredResult = allCourses.filter((courses) => courses.catagory.includes(searchValue));
+            if (filteredResult.length > 0) {
+                filteredResult.map((eachResult) => {
+                    let eachResultDiv = document.createElement('div');
+                    let imgDiv = document.createElement('div');
+                    let img = document.createElement('img');
+                    img.setAttribute('src', eachResult.img)
+
+                    imgDiv.appendChild(img);
+
+                    let a = document.createElement('a');
+                    a.setAttribute('href', eachResult.link);
+                    eachResultDiv.setAttribute('class', 'result-div');
+                    a.innerHTML = eachResult.name;
+
+                    eachResultDiv.appendChild(imgDiv)
+                    eachResultDiv.appendChild(a)
+
+                    resultContainerDiv.appendChild(eachResultDiv);
+                    return resultContainerDiv;
+                });
+
+                searchResult.appendChild(resultContainerDiv);
+            }
+        }
+    }, [searchValue]);
+
     return (
         <div className='navbar'>
             <div className='nav'>
                 <div className='nav-title'>
-                    Nora's Academy
+                    <div className='nav-img'>
+                        <a href="/#home-section">
+                            <img src={noraLogoOne} alt="logo" />
+                        </a>
+                    </div>
+                    <div className='search-bar-section'>
+                        <div className='search-bar'>
+                            <label htmlFor="search-value">
+                                <BsSearch />
+                            </label>
+                            <input id="search-value" type="search" name={searchValue} placeholder="find courses" onChange={(e) => handleSearch(e)} />
+                        </div>
+                        <div className='search-result'></div>
+                    </div>
                 </div>
-                <div>
-                    <a className='nav-link' href="/#home-section">Home</a>
-                    <a className='nav-link' href="/#courses-section">Courses</a>
-                    <a className='nav-link' href="/#contact-section">Contact</a>
+                <div className='nav-links'>
+                    <div className='nav-link'><a href="/#home-section">Home</a></div>
+                    <div className='nav-link'><a href="/#courses-section">Courses</a></div>
+                    <div className='nav-link'><a href="/#contact-section">Contact</a></div>
+                    <div className='nav-link'><a href="/">About Us</a></div>
                 </div>
             </div>
         </div>
